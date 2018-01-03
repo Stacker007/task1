807e765ee2a99f5614d279918ccd9a3ba1888b08
Stacker007 <34865131+Stacker007@users.noreply.github.com>
2018-01-03 19:29:11 +0300

    Update Task3.cpp

--- Task3.cpp
+++ Task3.cpp
@@ -13,10 +13,7 @@ int main (){
 	cout << "Эти числа делятся без остатка на следующие числа: "<< endl;
 	
 	for (int i=1; i<=b && i<=a; i++) 	if (a%i==0 && b%i==0) cout << i<< endl;
-	
-	
+
 return 0;
-	
-	
-	
+		
 }
