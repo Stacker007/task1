bec95411999d8f8fc50c6d5cf17b67a1b261202c
Stacker007 <34865131+Stacker007@users.noreply.github.com>
2018-01-03 19:22:48 +0300

    Create Task3.cpp

---
+++ Task3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+	setlocale (LC_ALL, "rus");
+	
+	int a,b;
+	cout<< "Введите два целых числа " ;
+	cin >> a>> b;
+	a=abs(a);
+	b=abs(b);
+	cout << "Эти числа делятся без остатка на следующие числа: "<< endl;
+	
+	for (int i=1; i<=b && i<=a; i++) 	if (a%i==0 && b%i==0) cout << i<< endl;
+	
+	
+return 0;
+	
+	
+	
+}
